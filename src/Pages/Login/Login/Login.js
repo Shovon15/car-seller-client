27129302518f1be3038b9666f96c879e6e4f462c
Shovon15 <shovonrnbd@gmail.com>
2022-11-24 00:33:00 +0600

    add signUp and Login page

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,12 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Login = () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit,
+    } = useForm();
+
+    const { loginError, setLoginError } = useState();
+
+    const handleLogin = () => {};
+
+    const [passwordShown, setPasswordShown] = useState(false);
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown);
+    };
+
     return (
-        <div>
-            <h1>login</h1>
+        <div className="h-[800px] flex justify-center items-center ">
+            <div className="w-96 p-7 rounded-xl shadow-xl">
+                <h2 className="text-2xl text-center font-bold text-orange-600">Login</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input
+                            type="text"
+                            {...register("email", {
+                                required: "Email Address is required",
+                            })}
+                            className="input input-bordered w-full max-w-xs"
+                        />
+                        {errors.email && <p className="text-red-600">{errors.email?.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs ">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <div className="relative">
+                            <input
+                                type={passwordShown ? "text" : "password"}
+                                {...register("password", {
+                                    required: "Password is required",
+                                    minLength: { value: 6, message: "Password must be 6 characters or longer" },
+                                })}
+                                className="input input-bordered w-full max-w-xs"
+                            />
+                            <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
+                                <span onClick={togglePassword} className="cursor-pointer">
+                                    {passwordShown === true ? <FaEye /> : <FaEyeSlash />}
+                                </span>
+                            </div>
+                        </div>
+                        {errors.password && <p className="text-red-600">{errors.password?.message}</p>}
+                        <label className="label">
+                            <span className="label-text">Forget Password?</span>
+                        </label>
+                    </div>
+                    <input
+                        className="btn bg-orange-500 hover:bg-orange-700 border-none w-full"
+                        value="Login"
+                        type="submit"
+                    />
+                    <div>{loginError && <p className="text-red-600">{loginError}</p>}</div>
+                </form>
+                <p className="p-4">
+                    New to Car Seller
+                    <Link className="text-primary px-2 hover:underline" to="/signupBuyer">
+                        Create new Account
+                    </Link>
+                </p>
+                <div className="divider">OR</div>
+                <button className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
 
 export default Login;
-<h1>login</h1>;
--- src/Pages/Login/SignUpBuyer/SignUpBuyer.js
+++ src/Pages/Login/SignUpBuyer/SignUpBuyer.js
@@ -1,9 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const SignUpBuyer = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+    const [signUpError, setSignUpError] = useState();
+
+    const handleSignUp = () => {};
+
+    // -------------------check buyer/seller--------------
+
+    const [userRole, setUserRole] = useState(true);
+    let user = "buyer";
+    const toggleUser = () => {
+        setUserRole(!userRole);
+    };
+    if (userRole === true) {
+        user = "buyer";
+    } else {
+        user = "seller";
+    }
+    console.log(user);
+    // -------------------show pass-----------
+    const [passwordShown, setPasswordShown] = useState(false);
+    const togglePassword = () => {
+        setPasswordShown(!passwordShown);
+    };
     return (
-        <div>
-            <h1>SignUpBuyer</h1>
+        <div className="h-[800px] flex justify-center items-center">
+            <div className="w-96 p-7">
+                <h2 className="text-2xl text-center font-bold text-orange-600">Sign Up</h2>
+
+                <span onClick={toggleUser} className="flex space-x-2 justify-center items-center p-2 text-orange-600">
+                    <button className="btn btn-outline btn-sm">{user} </button>
+                    <p className="font-bold">Account</p>
+                </span>
+
+                <form onSubmit={handleSubmit(handleSignUp)}>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input
+                            type="text"
+                            {...register("name", {
+                                required: "Name is Required",
+                            })}
+                            className="input input-bordered w-full max-w-xs"
+                        />
+                        {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input
+                            type="email"
+                            {...register("email", {
+                                required: "email is required",
+                            })}
+                            className="input input-bordered w-full max-w-xs"
+                        />
+                        {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+                    </div>
+                    <div className="form-control w-full max-w-xs ">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <div className="relative">
+                            <input
+                                type={passwordShown ? "text" : "password"}
+                                {...register("password", {
+                                    required: "Password is required",
+                                    minLength: { value: 6, message: "Password must be 6 characters long" },
+                                    pattern: {
+                                        value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/,
+                                        message: "Password must have uppercase, number and special characters",
+                                    },
+                                })}
+                                className="input input-bordered w-full max-w-xs"
+                            />
+                            <div className="absolute inset-y-0 right-0 pr-3 flex items-center">
+                                <span onClick={togglePassword} className="cursor-pointer">
+                                    {passwordShown === true ? <FaEye /> : <FaEyeSlash />}
+                                </span>
+                            </div>
+                        </div>
+                        {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+                    </div>
+                    <input
+                        className="btn bg-orange-500 hover:bg-orange-700 border-none w-full mt-4"
+                        value="Sign Up"
+                        type="submit"
+                    />
+                    {signUpError && <p className="text-red-600">{signUpError}</p>}
+                </form>
+                <p className="p-4">
+                    Already have an account
+                    <Link className="text-primary px-2 hover:underline" to="/login">
+                        Please Login
+                    </Link>
+                </p>
+                <div className="divider">OR</div>
+                <button className="btn btn-outline w-full">CONTINUE WITH GOOGLE</button>
+            </div>
         </div>
     );
 };
