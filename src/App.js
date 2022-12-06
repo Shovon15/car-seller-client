ddcaf629a4e23d47ef454533e893396b2143fc07
Shovon15 <shovonrnbd@gmail.com>
2022-12-06 15:13:16 +0600

    git repo change

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ import "react-photo-view/dist/react-photo-view.css";
 
 function App() {
     return (
-        <div className="max-w-screen-xl mx-auto bg-white dark:bg-slate-500 dark:text-white">
-            <RouterProvider router={router}></RouterProvider>
-            <Toaster />
+        // <div className="max-w-screen-xl mx-auto bg-white dark:bg-slate-500 dark:text-white">
+        //     <RouterProvider router={router}></RouterProvider>
+        //     <Toaster />
+        // </div>
+
+        <div className="bg-slate-100 dark:bg-slate-700 dark:text-white ">
+            <div className="max-w-[1440px] mx-auto ">
+                <RouterProvider router={router}></RouterProvider>
+                <Toaster />
+            </div>
         </div>
     );
 }
