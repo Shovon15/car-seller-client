fda8aa802ed850306449e62a13e3a64fad0d1b5a
Shovon15 <shovonrnbd@gmail.com>
2023-07-03 14:23:44 +0600

    useUser Modify

--- src/hooks/useUser.js
+++ src/hooks/useUser.js
@@ -3,24 +3,19 @@ import { useEffect, useState } from "react";
 const useUser = (email) => {
   const [isUser, setIsUser] = useState(false);
   const [isUserLoading, setIsUserLoading] = useState(true);
-  // console.log(isUser, email);
 
   useEffect(() => {
     if (email) {
       fetch(`https://y-shovon15.vercel.app/users/${email}`)
         .then((res) => res.json())
         .then((data) => {
-          // console.log(data);
           setIsUser(data);
           setIsUserLoading(false);
         });
     }
   }, [email]);
-  // console.log(isUser);
-  return [isUser, isUserLoading];
 
-  // const isUser = "shovon";
-  // return isUser;
+  return [isUser, isUserLoading];
 };
 
 export default useUser;
